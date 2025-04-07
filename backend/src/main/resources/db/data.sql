c1cd8e1b7fc149f1aa077b53e757d88ff4ebe359
YHCM <a2646399721@163.com>
2025-04-07 22:44:09 +0800

    数据库，出问题了

--- backend/src/main/resources/db/data.sql
+++ backend/src/main/resources/db/data.sql
@@ -1,20 +1,27 @@
 DELETE FROM users;
+DELETE FROM client_registration_requests;
 
 -- 插入 ROOT 用户（使用 root_seq）
 INSERT INTO users (user_id, username, password, role)
 VALUES (NEXT VALUE FOR root_seq, 'root', '$2a$10$uLbCvSxgSsTStJurm58Hber2jViqIjTwjFAhGP/IHwRBNJpvodjKa', 'ROOT');
 
-
 -- 插入 ADMIN 用户（使用 admin_seq）
 INSERT INTO users (user_id, username, password, role)
 VALUES (NEXT VALUE FOR admin_seq, 'admin', '$2a$10$vd9BtirJh6F.vphjwPSEIOc13BUoNeoTTx3VMdQc5K4cq6zi1t1hG', 'ADMIN');
-
 
 -- 插入 STAFF 用户（使用 staff_seq）
 INSERT INTO users (user_id, username, password, role)
 VALUES (NEXT VALUE FOR staff_seq, 'staff', '$2a$10$Tlfg/uzVpbyIihFwrdgkU.t3E6CeUIZD99BC5qh09r89yJtuFFgci', 'STAFF');
 
-
 -- 插入 CLIENT 用户（使用 client_seq）
 INSERT INTO users (user_id, username, password, role, company_name, phone_number)
 VALUES (NEXT VALUE FOR client_seq, 'client', '$2a$10$EnTQaiXERPXNHdDMz9.B1ueWSN17zaIFzywglwpqQRGSe/UTWiLIW', 'CLIENT', 'company', '1234567890');
+
+
+-- 插入客户注册申请信息（之后记得改密码）
+INSERT INTO client_registration_requests (request_id, username, password, commpany_name, phone_number) VALUES
+(1, 'client_request1', '123456', '1号公司', '11111'),
+(2, 'client_request2', '123456', '2号公司', '22222'),
+(3, 'client_request3', '123456', '3号公司', '33333'),
+(4, 'client_request4', '123456', '4号公司', '44444'),
+(5, 'client_request5', '123456', '5号公司', '55555');
--- backend/src/main/resources/db/schema.sql
+++ backend/src/main/resources/db/schema.sql
@@ -1,14 +1,20 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS client_registration_requests;
 
 DROP SEQUENCE IF EXISTS root_seq;
 DROP SEQUENCE IF EXISTS admin_seq;
 DROP SEQUENCE IF EXISTS staff_seq;
 DROP SEQUENCE IF EXISTS client_seq;
 
+DROP SEQUENCE IF EXISTS request_seq;
+
+-- 用户 ID 序列
 CREATE SEQUENCE root_seq START WITH 10001;      -- ROOT 用户的 ID，从 10001 开始
 CREATE SEQUENCE admin_seq START WITH 20001;     -- ADMIN 用户的 ID，从 20001 开始
 CREATE SEQUENCE staff_seq START WITH 30001;     -- STAFF 用户的 ID，从 30001 开始
 CREATE SEQUENCE client_seq START WITH 40001;    -- CLIENT 用户的 ID，从 40001 开始
+
+CREATE SEQUENCE request_seq START WITH 10001;   -- 客户注册的申请 ID 序列，从 10001 开始
 
 -- 用户表（包括超级管理员，管理员，员工，客户）
 CREATE TABLE users (
@@ -16,7 +22,19 @@ CREATE TABLE users (
     username VARCHAR(127) UNIQUE NOT NULL COMMENT '用户名，唯一，非空',
     password VARCHAR(63) NOT NULL COMMENT '密码，非空',
     role ENUM('ROOT', 'ADMIN', 'STAFF', 'CLIENT') NOT NULL COMMENT '用户的角色，非空',
-    company_name VARCHAR(127) COMMENT '公司名字，仅客户需要',
+    company_name VARCHAR(127) COMMENT '公司名称，仅客户需要',
     phone_number VARCHAR(31) COMMENT '电话号码，目前仅客户需要',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '用户添加的时间'
 );
+
+-- 客户注册申请表
+CREATE TABLE client_registration_requests (
+    request_id BIGINT PRIMARY KEY COMMENT '主键，申请 ID',
+    username VARCHAR(127) UNIQUE NOT NULL COMMENT '用户名，唯一，非空',
+    password VARCHAR(63) NOT NULL COMMENT '密码，非空',
+    company_name VARCHAR(127) NOT NULL COMMENT '公司名称，非空',
+    phone_number VARCHAR(31) NOT NULL COMMENT '电话号码，非空',
+    status ENUM('PENDING', 'APPROVED', 'REJECTED') DEFAULT 'PENDING' COMMENT '申请状态，默认是待审批',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '申请请求的创建时间',
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '申请的最后更新时间（处理时间）'
+)
