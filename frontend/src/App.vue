8f59e0a04220dd2467d99fb3acb2e36df3497ce1
琳 <17641602515@163.com>
2025-04-24 17:11:06 +0800

    首页一点小改动

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,3 +8,11 @@
 import MainView from '@/views/MainView.vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 </script>
+
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+  background-color: #ffffff;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <template>
-  <h1>主页</h1>
   <div class="home-view">
     <!-- 轮播浮窗 -->
     <el-carousel trigger="click" :interval="5000" arrow="always" class="carousel-wrapper">
@@ -13,17 +12,30 @@
           :body-style="{ padding: '20px', width: '70%', height: '300px' }"
         >
           <div class="carousel-card-content">
-            <!-- 标题居中显示 -->
             <div class="carousel-card-text">
               <h3>{{ item.title }}</h3>
               <p>{{ item.subtitle }}</p>
             </div>
-            <!-- 放大图片 -->
             <img :src="item.image" alt="banner" class="carousel-card-image" />
           </div>
         </el-card>
       </el-carousel-item>
     </el-carousel>
+
+    <!-- 功能展示区 -->
+    <div class="new-cards">
+      <h2>请选择服务</h2>
+      <div class="card-container">
+        <el-card v-for="(card, index) in newCards" :key="index" class="new-card" @click="goToPage(card.link)">
+          <div class="new-card-content">
+            <img :src="card.image" alt="card" class="new-card-image" />
+            <div class="new-card-text">
+              <h3>{{ card.title }}</h3>
+            </div>
+          </div>
+        </el-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,6 +46,10 @@ import { useRouter } from 'vue-router'
 import authImage1 from '@/assets/images/auth-1.png'
 import authImage2 from '@/assets/images/auth-2.png'
 import authImage3 from '@/assets/images/auth-3.png'
+import newCardImage1 from '@/assets/images/auth-1.png'
+import newCardImage2 from '@/assets/images/auth-1.png'
+import newCardImage3 from '@/assets/images/auth-1.png'
+
 
 // 轮播项的数据
 const carouselItems = ref([
@@ -55,6 +71,25 @@ const carouselItems = ref([
     image: authImage3,
     link: '/',
   },
+])
+
+// 功能卡片,后续再改
+const newCards = ref([
+  {
+    title: "设备管理",
+    image: newCardImage1,
+    link: "/equipments"
+  },
+  {
+    title: "预订会议室",
+    image: newCardImage2,
+    link: "/"
+  },
+  {
+    title: "管理会议室",
+    image: newCardImage3,
+    link: "/"
+  }
 ])
 
 const router = useRouter()
@@ -117,5 +152,48 @@ const goToPage = (link) => {
   height: auto;
   border-radius: 8px;
   margin-top: 10px;
+}
+
+.new-cards {
+  margin-top: 40px;
+}
+
+.card-container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.new-card {
+  width: 30%;
+  cursor: pointer;
+  box-shadow: 0 4px 10px rgb(0, 0, 0);
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.new-card-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+}
+
+.new-card-image {
+  width: 100%;
+  max-width: 150px;
+  height: auto;
+  border-radius: 8px;
+}
+
+.new-card-text {
+  text-align: center;
+  margin-top: 10px;
+}
+
+.new-card-text h3 {
+  font-size: 18px;
+  font-weight: bold;
 }
 </style>
