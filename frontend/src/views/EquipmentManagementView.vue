9f3994f0c0571340191beccb90047a9b5e0408b1
琳 <17641602515@163.com>
2025-04-24 14:47:18 +0800

    设备管理的删除、新增、修改

--- frontend/src/views/EquipmentManagementView.vue
+++ frontend/src/views/EquipmentManagementView.vue
@@ -8,17 +8,31 @@
     <!-- 设备表格 -->
     <el-table :data="currentPageData" v-loading="loading" style="width: 100%" border stripe>
       <el-table-column prop="equipmentId" label="ID" />
-      <el-table-column prop="equipmentName" label="设备名称" />
-      <el-table-column prop="description" label="描述" />
-      <el-table-column prop="additionalPrice" label="额外价格" />
+      <el-table-column prop="equipmentName" label="设备名称">
+        <template #default="{ row }">
+          <el-input v-if="row.equipmentId === editingEquipment.equipmentId" v-model="editingEquipment.equipmentName" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="description" label="描述">
+        <template #default="{ row }">
+          <el-input v-if="row.equipmentId === editingEquipment.equipmentId" v-model="editingEquipment.description" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="additionalPrice" label="额外价格">
+        <template #default="{ row }">
+          <el-input-number v-if="row.equipmentId === editingEquipment.equipmentId" v-model="editingEquipment.additionalPrice" :min="0" :precision="2" controls-position="right" />
+        </template>
+      </el-table-column>
       <el-table-column label="操作" width="160">
         <template #default="{ row }">
-          <el-button size="small" type="warning" @click="editEquipment(row)" :loading="loading">
-            修改
-          </el-button>
-          <el-button size="small" type="danger" @click="deleteEquipment(row.equipmentId)" :loading="loading">
-            删除
-          </el-button>
+          <span v-if="row.equipmentId === editingEquipment.equipmentId">
+            <el-button size="small" type="success" :loading="loading" @click="saveEquipment">保存</el-button>
+            <el-button size="small" type="info" @click="resetEditingEquipment">取消</el-button>
+          </span>
+          <span v-else>
+            <el-button size="small" type="warning" @click="editEquipment(row)">修改</el-button>
+            <el-button size="small" type="danger" @click="deleteEquipment(row.equipmentId)" :loading="loading">删除</el-button>
+          </span>
         </template>
       </el-table-column>
     </el-table>
@@ -36,7 +50,7 @@
     />
 
     <!-- 新增设备弹框 -->
-    <el-dialog :visible.sync="addDialogVisible" title="新增设备" @close="resetAddForm">
+    <el-dialog v-model="addDialogVisible" title="新增设备" @close="resetAddForm">
       <el-form :model="newEquipment" ref="addForm" label-width="120px">
         <el-form-item label="设备名称" :rules="[{ required: true, message: '请输入设备名称', trigger: 'blur' }]">
           <el-input v-model="newEquipment.equipmentName" />
@@ -58,7 +72,6 @@
 
 <script setup>
 import { ref, onMounted, computed } from 'vue'
-import { ElMessage, ElMessageBox, ElDialog, ElForm, ElFormItem, ElInput, ElButton } from 'element-plus'
 import { useApi } from '@/composables/useApi'
 import { usePagination } from '@/composables/usePagination'
 
@@ -67,18 +80,17 @@ const http = useApi()
 // 设备数据
 const equipmentList = ref([]) // 设备列表
 const loading = ref(false) // 加载状态
-
-// 分页设置
-const { pagination, handleCurrentChange, handleSizeChange, getCurrentPageData, updateTotal } = usePagination()
-const currentPageData = computed(() => getCurrentPageData(equipmentList.value))
-
-// 新增设备弹框
-const addDialogVisible = ref(false)
+const editingEquipment = ref({ equipmentId: null, equipmentName: '', description: '', additionalPrice: 0 })
 const newEquipment = ref({
   equipmentName: '',
   description: '',
   additionalPrice: null
 })
+const addDialogVisible = ref(false)
+
+// 分页设置
+const { pagination, handleCurrentChange, handleSizeChange, getCurrentPageData, updateTotal } = usePagination()
+const currentPageData = computed(() => getCurrentPageData(equipmentList.value))
 
 // 获取设备数据
 const getEquipment = async () => {
@@ -104,15 +116,44 @@ const deleteEquipment = async (equipmentId) => {
       type: 'warning'
     })
     const response = await http.delete(`/equipment/${equipmentId}`)
-    if (response.status === 204) {
+    if (response.code === 204) {
       ElMessage.success('设备删除成功')
-      getEquipment() 
+      getEquipment()
     } else {
       ElMessage.error('设备删除失败')
     }
   } catch (error) {
-    ElMessage.error('删除设备失败')
+    ElMessage.info('取消删除设备')
   }
+}
+
+// 编辑设备
+const editEquipment = (row) => {
+  editingEquipment.value = { ...row }  
+}
+
+// 保存修改的设备
+const saveEquipment = async () => {
+  loading.value = true
+  try {
+    const response = await http.put('/equipment', editingEquipment.value)
+    if (response.code === 200) {
+      ElMessage.success('设备修改成功')
+      resetEditingEquipment()
+      getEquipment()  // 刷新设备列表
+    } else {
+      ElMessage.error('设备修改失败')
+    }
+  } catch (error) {
+    ElMessage.error('设备修改失败')
+  } finally {
+    loading.value = false
+  }
+}
+
+// 取消编辑
+const resetEditingEquipment = () => {
+  editingEquipment.value = { equipmentId: null, equipmentName: '', description: '', additionalPrice: 0 }
 }
 
 // 打开新增设备弹窗
@@ -133,7 +174,7 @@ const resetAddForm = () => {
 const addEquipment = async () => {
   try {
     const response = await http.post('/equipment', newEquipment.value)
-    if (response.data.code === 200) {
+    if (response.code === 201) {
       ElMessage.success('设备添加成功')
       addDialogVisible.value = false
       getEquipment()  // 刷新设备列表
