a38e774f9b100ae8337d0fc5eb11108e75028ebe
matrixd125 <d9_125@163.com>
2025-04-29 12:00:30 +0800

    -

--- backend/src/main/java/com/example/entity/Order.java
+++ backend/src/main/java/com/example/entity/Order.java
@@ -25,5 +25,6 @@ public class Order {
         order.setBookingDate(request.getBookingDate());
         order.setStartHour(request.getStartHour());
         order.setEndHour(request.getEndHour());
+        return order;
     }
 }
