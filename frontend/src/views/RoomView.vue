8f61b8361860c2b09ebe7f262a1f78f271de3826
matrixd125 <d9_125@163.com>
2025-04-29 11:21:01 +0800

    会议室界面显示价格和设备

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -11,29 +11,22 @@
             {{ room.roomStatus ? '可用' : '不可用' }}
           </el-tag>
         </div>
-        <el-button
-          v-if="isStaff"
-          size="small"
-          :type="room.roomStatus ? 'danger' : 'success'"
-          :loading="loading"
-          @click="toggleRoomStatus(room.roomId)"
-        >
+        <div class="info">
+          <span>每小时租赁价格：{{ room.price }}</span><br />
+          <span class="ellipsis-single" :title="room.equipments">配置：{{ room.equipments }}</span>
+        </div>
+        <el-button v-if="isStaff" size="small" :type="room.roomStatus ? 'danger' : 'success'" :loading="loading"
+          @click="toggleRoomStatus(room.roomId)">
           {{ room.roomStatus ? '设为不可用' : '设为可用' }}
         </el-button>
       </div>
     </div>
 
     <!-- 分页组件 -->
-    <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="pagination.currentPage"
-      :page-sizes="[10, 15, 20, 25]"
-      :page-size="pagination.pageSize"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="pagination.total"
-      style="margin-top: 20px; justify-content: flex-end"
-    />
+    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+      :current-page="pagination.currentPage" :page-sizes="[10, 15, 20, 25]" :page-size="pagination.pageSize"
+      layout="total, sizes, prev, pager, next, jumper" :total="pagination.total"
+      style="margin-top: 20px; justify-content: flex-end" />
   </div>
 </template>
 
@@ -51,13 +44,16 @@ const { pagination, handleCurrentChange, handleSizeChange, updateTotal, getCurre
   usePagination()
 
 // 是否是员工
-const isStaff = computed(() => ['ADMIN','STAFF'].includes(userStore.userInfo.role))
+const isStaff = computed(() => ['ADMIN', 'STAFF'].includes(userStore.userInfo.role))
 
 // 获取会议室列表
 const getRooms = async () => {
   loading.value = true
   try {
-    const response = await http.get('/rooms')
+    const response = await http.get('/rooms/info')
+    for (let room of response.data) {
+      room.equipments = formatEquipments(room.equipments)
+    }
     rooms.value = response.data
     updateTotal(rooms.value.length)
   } catch (error) {
@@ -88,6 +84,10 @@ const toggleRoomStatus = async (roomId) => {
   }
 }
 
+const formatEquipments = (equipments) => {
+  return equipments.map(e => e.equipmentName).join('/')
+}
+
 onMounted(() => {
   getRooms()
 })
@@ -102,7 +102,8 @@ onMounted(() => {
 }
 
 .room-status {
-  flex: 0 0 calc(20% - 20px); /* 每行 5 个 */
+  flex: 0 0 calc(20% - 20px);
+  /* 每行 5 个 */
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -126,5 +127,19 @@ onMounted(() => {
 
 .el-button {
   margin-top: 10px;
+}
+
+.info {
+  font-size: small;
+}
+
+.ellipsis-single {
+  width: auto;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  line-clamp: 1;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
 }
 </style>
